99edb23962695e2c6bbba0fe2434e6a4065035f3
Juan Guajardo <gigfmedia@icloud.com>
2026-04-28 19:47:05 -0400

    UPDATE: Register all missing blog routes + complete sitemap (23 articles)

--- src/App.tsx
+++ src/App.tsx
@@ -93,6 +93,17 @@ const DespidoSinMotivoArticle = lazy(() => import('./pages/blog/me-pueden-desped
 const LeyDevuelvemeMiCasaArticle = lazy(() => import('./pages/blog/ley-devuelveme-mi-casa-chile-2026'));
 const ReviewPage = lazy(() => import('./pages/ReviewPage'));
 const DeudaPensionArticle = lazy(() => import('./pages/blog/deuda-pension-alimentos-chile-2026'));
+const DerechoArrendamientoArticle = lazy(() => import('./pages/blog/derecho-arrendamiento-chile-guia-completa-2026'));
+const ContratoArriendoArticle = lazy(() => import('./pages/blog/contrato-de-arriendo-chile-2026'));
+const CuantosMesesArriendoArticle = lazy(() => import('./pages/blog/cuantos-meses-debo-arriendo-para-que-me-desalojen-chile-2026'));
+const NoDevuelvenGarantiaArticle = lazy(() => import('./pages/blog/no-devuelven-garantia-arriendo-chile-2026'));
+const MePuedenDemandarArriendoArticle = lazy(() => import('./pages/blog/me-pueden-demandar-por-no-pagar-el-arriendo-chile-2026'));
+const QuePasaSinContratoArticle = lazy(() => import('./pages/blog/que-pasa-si-no-tengo-contrato-de-arriendo-chile-2026'));
+const ReajusteArriendoArticle = lazy(() => import('./pages/blog/reajuste-arriendo-ipc-chile-2026'));
+const TacitaReconduccionArticle = lazy(() => import('./pages/blog/tacita-reconduccion-chile-2026'));
+const DicomArriendoArticle = lazy(() => import('./pages/blog/dicom-deuda-arriendo-chile-2026'));
+const DespidoInjustificadoArticle = lazy(() => import('./pages/blog/despido-injustificado-chile-2026'));
+const AnosServicioArticle = lazy(() => import('./pages/blog/cuanto-me-corresponde-anos-de-servicio-chile-2026'));
 
 // Create a single QueryClient instance
 const queryClient = new QueryClient({
@@ -373,6 +384,17 @@ const AppContent = () => {
               <Route path="/blog/me-pueden-despedir-sin-motivo-chile-2026" element={<DespidoSinMotivoArticle />} />
               <Route path="/blog/ley-devuelveme-mi-casa-chile-21461-2026" element={<LeyDevuelvemeMiCasaArticle />} />
               <Route path="/blog/deuda-pension-alimentos-chile-2026" element={<DeudaPensionArticle />} />
+              <Route path="/blog/derecho-arrendamiento-chile-guia-completa-2026" element={<DerechoArrendamientoArticle />} />
+              <Route path="/blog/contrato-de-arriendo-chile-2026" element={<ContratoArriendoArticle />} />
+              <Route path="/blog/cuantos-meses-debo-arriendo-para-que-me-desalojen-chile-2026" element={<CuantosMesesArriendoArticle />} />
+              <Route path="/blog/no-devuelven-garantia-arriendo-chile-2026" element={<NoDevuelvenGarantiaArticle />} />
+              <Route path="/blog/me-pueden-demandar-por-no-pagar-el-arriendo-chile-2026" element={<MePuedenDemandarArriendoArticle />} />
+              <Route path="/blog/que-pasa-si-no-tengo-contrato-de-arriendo-chile-2026" element={<QuePasaSinContratoArticle />} />
+              <Route path="/blog/reajuste-arriendo-ipc-chile-2026" element={<ReajusteArriendoArticle />} />
+              <Route path="/blog/tacita-reconduccion-chile-2026" element={<TacitaReconduccionArticle />} />
+              <Route path="/blog/dicom-deuda-arriendo-chile-2026" element={<DicomArriendoArticle />} />
+              <Route path="/blog/despido-injustificado-chile-2026" element={<DespidoInjustificadoArticle />} />
+              <Route path="/blog/cuanto-me-corresponde-anos-de-servicio-chile-2026" element={<AnosServicioArticle />} />
               <Route path="/review" element={<ReviewPage />} />
              
 
